00f586e591560d79ad0d5685dd06e70e069d7e59
dianjiu <dianjiu@dianjiu.cc>
2021-05-21 09:58:27 +0800

    timeout=60000

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,7 @@ const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 120000 // request timeout
+  timeout: 60000 // request timeout
 })
 
 // request interceptor
